a505257c3b3d7f4b1906c4abc8a8c93c409b493f
mspruc <Lqekrmal1>
2020-11-04 11:46:06 +0100

    made fetchInput

---
+++ src/Profile.java
@@ -0,0 +1,4 @@
+public class Profile {
+    //here we could put the age, work, dob etc. of the person chatting with bot
+    Profile(){}
+}
--- src/QuestionHandler.java
+++ src/QuestionHandler.java
@@ -5,6 +5,7 @@ public class QuestionHandler {
     String[] questionWords = {"who","why","what", "how"};
     String[] youWords = {"you","your","yours"};
     String[] meWords = {"me","myself","my","I"};
+    String[] badWords = {"Fuck","Shit","Fucking","reatrad"};
     ArrayList<String> response = new ArrayList<>();
 
     boolean isFinished;
@@ -12,6 +13,9 @@ public class QuestionHandler {
     void loop(){
         while(!isFinished){
             fetchInput();
+            analyseResponse();
+
+            response = new ArrayList<>();
         }
     }
 
@@ -27,7 +31,6 @@ public class QuestionHandler {
 
             if(analyseChar != ' '){ //if not a space add it to the word
                 wordConstructor.append(analyseChar);
-                System.out.println(analyseChar);
             } else { //if it is a space save the word and clear it.
                 response.add(responseIterator,String.valueOf(wordConstructor));
                 wordConstructor = new StringBuilder();
@@ -43,5 +46,49 @@ public class QuestionHandler {
     }
 
     void analyseResponse(){
+        for (String word : response) {
+
+            boolean isSwearWord = false;
+            for (String swear : badWords) {
+                if (word.equalsIgnoreCase(swear)) {
+                    isSwearWord = true;
+                    break;
+                }
+            }
+
+            boolean isQuestionWord = false;
+            for (String question: questionWords) {
+                if (word.equalsIgnoreCase(question)) {
+                    isQuestionWord = true;
+                    break;
+                }
+            }
+
+            boolean isYouWord = false;
+            for (String question: youWords) {
+                if (word.equalsIgnoreCase(question)) {
+                    isYouWord = true;
+                    break;
+                }
+            }
+
+            boolean isMeWord = false;
+            for (String question: meWords) {
+                if (word.equalsIgnoreCase(question)) {
+                    isMeWord = true;
+                    break;
+                }
+            }
+
+            if (isSwearWord) {
+                System.out.println("DONT SWEAR YOU RETARD.");
+            } else if (isQuestionWord) {
+                System.out.println("Wow, asking me a question are you?");
+            } else if (isYouWord) {
+                System.out.println("We are talking about you, not me.");
+            } else if (isMeWord){
+                System.out.println("Wow, you're really self-centered.");
+            }
+        }
     }
 }
