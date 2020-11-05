f88543b42422f5b967ab9a22a3ce843814f36d52
Frede <fredboy@live.dk>
2020-11-05 10:09:49 +0100

    Made responses good good

--- src/QuestionHandler.java
+++ src/QuestionHandler.java
@@ -1,16 +1,19 @@
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 public class QuestionHandler {
+    String[] heyWords = {"greetings" , "hi", "hello", "hey"};
     String[] questionWords = {"who","why","what", "how"};
     String[] youWords = {"you","your","yours"};
-    String[] meWords = {"me","myself","my","I"};
-    String[] badWords = {"Fuck","Shit","Fucking","reatrad"};
+    String[] meWords = {"me","myself","my","I", "I'm", "Im"};
+    String[] badWords = {"Fuck","Shit","Fucking","retard"};
     ArrayList<String> response = new ArrayList<>();
 
     boolean isFinished;
+    int swearCounter;
 
-    void loop(){
+    void loop() throws InterruptedException {
         while(!isFinished){
             fetchInput();
             analyseResponse();
@@ -45,50 +48,85 @@ public class QuestionHandler {
         }
     }
 
-    void analyseResponse(){
+
+    Boolean isAWord(String[]wordList, String stringWord){
+        for (String word : wordList) {
+            if (word.equalsIgnoreCase(stringWord)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    void analyseResponse() throws InterruptedException {
+        int randomInt = (int) (Math.random() * 3);
+        boolean isHeyWord = false;
+        boolean isSwearWord = false;
+        boolean isQuestionWord = false;
+        boolean isYouWord = false;
+        boolean isMeWord = false;
+
         for (String word : response) {
 
-            boolean isSwearWord = false;
-            for (String swear : badWords) {
-                if (word.equalsIgnoreCase(swear)) {
-                    isSwearWord = true;
-                    break;
+            if(!isHeyWord) {
+                isHeyWord = isAWord(heyWords, word);
+            }
+            if(!isSwearWord) {
+                isSwearWord = isAWord(badWords, word);
+            }
+            if(!isQuestionWord) {
+                isQuestionWord = isAWord(questionWords, word);
+            }
+            if(!isYouWord) {
+                isYouWord = isAWord(youWords, word);
+            }
+            if(!isMeWord) {
+                isMeWord = isAWord(meWords, word);
+            }
+        }
+
+            if(isHeyWord){
+
+                if(randomInt == 0) {
+                    System.out.println("Hello to you too. ");
+                }
+                if(randomInt == 1){
+                    System.out.println("Hi there. ");
+                }
+                if(randomInt == 2) {
+                    System.out.println("Hey. ");
                 }
             }
-
-            boolean isQuestionWord = false;
-            for (String question: questionWords) {
-                if (word.equalsIgnoreCase(question)) {
-                    isQuestionWord = true;
-                    break;
-                }
-            }
-
-            boolean isYouWord = false;
-            for (String question: youWords) {
-                if (word.equalsIgnoreCase(question)) {
-                    isYouWord = true;
-                    break;
-                }
-            }
-
-            boolean isMeWord = false;
-            for (String question: meWords) {
-                if (word.equalsIgnoreCase(question)) {
-                    isMeWord = true;
-                    break;
-                }
-            }
-
             if (isSwearWord) {
-                System.out.println("DONT SWEAR YOU RETARD.");
-            } else if (isQuestionWord) {
-                System.out.println("Wow, asking me a question are you?");
-            } else if (isYouWord) {
-                System.out.println("We are talking about you, not me.");
-            } else if (isMeWord){
-                System.out.println("Wow, you're really self-centered.");
+                swearCounter++;
+                if(swearCounter > 1){
+                    System.out.println("You swore again. I'm done chatting. ");
+                    TimeUnit.SECONDS.sleep(1);
+                    System.exit(0);
+                }
+                System.out.println("Please don't swear. This is your last chance ");
+            }
+            if (isQuestionWord && !isYouWord && !isMeWord) {
+                if (randomInt == 0){
+                    System.out.println("Wow, asking me a question are you? ");
+                }
+                if (randomInt == 1){
+                    System.out.println("What do you think? ");
+                }
+                if (randomInt == 2){
+                    System.out.println("I don't know. Ask someone else.");
+                }
+            }
+            if (isYouWord) {
+                System.out.println("We are talking about you, not me. ");
+            }
+            if (isMeWord){
+                System.out.println("Wow, you're really self-centered. ");
+            }
+            if (!isHeyWord && !isMeWord && !isYouWord && !isQuestionWord && !isSwearWord){
+                System.out.println("How are you?");
             }
         }
     }
-}
+
+
